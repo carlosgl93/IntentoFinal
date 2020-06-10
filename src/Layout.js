944bb5d379d12394f0e73636d8012dd8bec0f32e
kanogl <cgumucio93@gmail.com>
2020-06-10 18:57:07 -0400

    primer commit, limpio

---
+++ src/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Layout.css';
+
+function Layout() {
+  return (
+    <div className="Layout">
+      <h1>This is my layout</h1> 
+    </div>
+  );
+}
+
+export default Layout;
